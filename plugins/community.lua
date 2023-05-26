c0ec979759141d10d8dda88496f2bbaf2fdea9db
Tony Fischer <tony@getml.com>
2023-05-26 16:32:01 +0200

    add codeium as community option

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,6 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.completion.codeium-vim" }, 
   { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.colorscheme.nightfox" },
   -- { import = "astrocommunity.colorscheme.everforest" },
