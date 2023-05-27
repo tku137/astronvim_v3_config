5896412be51d4b1e167dffb915b14341988fc285
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2023-05-27 22:51:39 +0200

    add new plugins and streamline community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,19 +4,36 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  -- { import = "astrocommunity.completion.codeium-vim" }, 
+  -- color schemes
   { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.colorscheme.nightfox" },
   -- { import = "astrocommunity.colorscheme.everforest" },
+
+  -- code completion
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.completion.codeium-vim" }, 
+
+  -- editing support
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
-  { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
-  -- { import = "astrocommunity.note-taking.obsidian-nvim" },
   -- { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
   -- { import = "astrocommunity.git.git-blame-nvim" },
   -- { import = "astrocommunity.markdown-and-latex.glow-nvim" },
+  -- { import = "astrocommunity.note-taking.obsidian-nvim" },
+  -- { import = "astrocommunity.editing-support.refactoring-nvim"}
+
+  -- motion
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.motion.harpoon" },
+  
+  -- packs
   -- { import = "astrocommunity.pack.python" },
   -- { import = "astrocommunity.pack.markdown" },
+  -- { import = "astrocommunity.pack.lua" },
+
+  -- others
+  -- helping you establish good command workflow and habit
+  -- { import = "astrocommunity.workflow.hardtime-nvim" },
 }
