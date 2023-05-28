9cdb1baf678805e8eb85a8d7a0f4cb323507d97d
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2023-05-28 23:00:44 +0200

    insert todo-comments commands into which key

--- mappings.lua
+++ mappings.lua
@@ -16,6 +16,7 @@ return {
       desc = "Pick to close",
     },
     -- better buffer navigation
+
     -- sets shift+l/h to switch buffers with option of a count before the command
     ["<S-l>"] = {
       function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
@@ -28,14 +29,23 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    
     -- some plugin mappings
     -- if lsp_lines is installed, this toggles it on or off
     -- ["<leader>ll"] = {require("lsp_lines").toggle, desc = "Toggle lsp lines",},
+    
     -- put aerial shortcuts under the 'find' menu
     -- ["<leader>fs"] = { "<cmd>Telescope aerial<cr>", desc = "Search symbols" },
     -- ["<leader>fS"] = { "<cmd>AerialToggle<cr>", desc = "Symbols outline" },
+
+    -- put todo-comments under trouble group
+    ["<leader>xt"] = { "<cmd>TodoTelescope<cr>", desc = "Open Todos in Telescope" },
+    ["<leader>xT"] = { "<cmd>TodoTrouble<cr>", desc = "Open Todos in Trouble" },
+    ["<leader>xQ"] = { "<cmd>TodoQuickFix<cr>", desc = "Quickfix List (Todo)" },
+    ["<leader>xL"] = { "<cmd>TodoLocList<cr>", desc = "Location List (Todo)" },
   },
   t = {
     -- setting a mapping to false will disable it
