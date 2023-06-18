aa4d54c7ce767ea7c5e14ffec33e15b3acf92a00
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2023-06-18 21:55:50 +0200

    disable send-to-term

--- plugins/user.lua
+++ plugins/user.lua
@@ -40,10 +40,10 @@ return {
   --     }
   --   end,
   -- },
-  {
-    "mtikekar/nvim-send-to-term",
-    init = function() vim.g.send_disable_mapping = true end,
-    keys = { "<Plug>Send", "<Plug>SendLine" },
-    cmd = "SendHere",
-  },
+  -- {
+  --   "mtikekar/nvim-send-to-term",
+  --   init = function() vim.g.send_disable_mapping = true end,
+  --   keys = { "<Plug>Send", "<Plug>SendLine" },
+  --   cmd = "SendHere",
+  -- },
 }
