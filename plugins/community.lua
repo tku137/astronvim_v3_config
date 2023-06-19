d33a7de982ebd75219ff00f314667bb0fd4c73f6
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2023-06-19 18:09:39 +0200

    disable mini.move

--- plugins/community.lua
+++ plugins/community.lua
@@ -78,7 +78,7 @@ return {
   { import = "astrocommunity.motion.flit-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   -- { import = "astrocommunity.motion.mini-surround" },
-  { import = "astrocommunity.motion.mini-move" },
+  -- { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.motion.harpoon" },
 
   -- packs
