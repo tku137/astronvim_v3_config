34394965bdc9ec82457ed9b2a6c4cdb23651a63b
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2023-06-21 21:38:58 +0200

    use native lsp inlay hints

--- options.lua
+++ options.lua
@@ -21,6 +21,7 @@ return {
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
     resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
+    inlay_hints_enabled = true, -- use native lsp inlay hints
   },
 }
 -- If you need more control, you can use the function()...end notation
