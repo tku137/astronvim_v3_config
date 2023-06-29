51f4d0b6be321eaf5044ecc4a7539ceedb3fc11e
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2023-06-29 21:21:52 +0200

    disable neoclip

--- plugins/neoclip.lua
+++ plugins/neoclip.lua
@@ -1,11 +1,12 @@
-return {
-    "AckslD/nvim-neoclip.lua",
-    init = function() require('telescope').load_extension('neoclip') end,
-    event = { "User AstroFile" },
-    dependencies = {
-      -- you'll need at least one of these
-      {'nvim-telescope/telescope.nvim'},
-      -- {'ibhagwan/fzf-lua'},
-    },
-    config = function() require("neoclip").setup() end,
-}
+return {}
+-- return {
+--     "AckslD/nvim-neoclip.lua",
+--     init = function() require('telescope').load_extension('neoclip') end,
+--     event = { "User AstroFile" },
+--     dependencies = {
+--       -- you'll need at least one of these
+--       {'nvim-telescope/telescope.nvim'},
+--       -- {'ibhagwan/fzf-lua'},
+--     },
+--     config = function() require("neoclip").setup() end,
+-- }
