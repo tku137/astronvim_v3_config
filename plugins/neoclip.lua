88661c0df8e9c1003566a2e03ce8490be8cf2971
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2023-07-16 23:10:22 +0200

    finally fix neoclip slow startup

--- plugins/neoclip.lua
+++ plugins/neoclip.lua
@@ -1,12 +1,14 @@
 -- return {}
 return {
     "AckslD/nvim-neoclip.lua",
-    init = function() require('telescope').load_extension('neoclip') end,
     event = { "User AstroFile" },
     dependencies = {
       -- you'll need at least one of these
       {'nvim-telescope/telescope.nvim'},
       -- {'ibhagwan/fzf-lua'},
     },
-    config = function() require("neoclip").setup() end,
+    config = function()
+        require("neoclip").setup()
+        require('telescope').load_extension('neoclip')
+    end,
 }
