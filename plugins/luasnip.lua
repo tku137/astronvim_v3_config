4c97d6a1b135534a83872d17bd06f290d1d00a03
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2023-07-18 21:35:26 +0200

    fix LuaSnip

--- plugins/luasnip.lua
+++ plugins/luasnip.lua
@@ -1,11 +1,16 @@
 return {
-  "L3MON4D3/LuaSnip",
-  config = function(plugin, opts)
-    require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-    require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } } -- load snippets paths
-    -- add more custom luasnip configuration such as filetype extend or custom snippets
-    local luasnip = require "luasnip"
-    luasnip.filetype_extend("javascript", { "javascriptreact" })
-  end,
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      -- include the default astronvim config that calls the setup call
+      require "plugins.configs.luasnip"(plugin, opts)
+      -- load snippets paths
+      require("luasnip.loaders.from_vscode").lazy_load {
+        -- this can be used if your configuration lives in ~/.config/nvim
+        -- if your configuration lives in ~/.config/astronvim, the full path
+        -- must be specified in the next line
+        paths = { "./lua/user/snippets" }
+      }
+    end,
+  },
 }
-
